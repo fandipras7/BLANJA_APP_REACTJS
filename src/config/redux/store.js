e6fadd5638e6fbbe9ae264906b54d3da2cb1d2d2
fandipras7 <fandiprasetyopro@gmail.com>
2022-07-24 17:10:52 +0700

    change to redux persist

--- src/config/redux/store.js
+++ src/config/redux/store.js
@@ -2,7 +2,16 @@ import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducer/rootReducer";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const persistConfig = {
+    key: 'root',
+    storage
+}
 
-export default store;
+const persitedReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(persitedReducer, applyMiddleware(thunk, logger));
+const persistor = persistStore(store)
+
+export {store, persistor};
--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from '../src/config/redux/store'
+import { store, persistor } from '../src/config/redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <Provider store={store}>
+  //   <App />
+  //   </Provider>
+  // </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
